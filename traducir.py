607433bb55321b79262a1163649e1583fdc5b10f
Juan de Frutos Jiménez <71391572+Juan-de-Frutos@users.noreply.github.com>
2026-09-11 00:10:40 +0200

    Change translator from Google to MyMemory

--- traducir.py
+++ traducir.py
@@ -1,17 +1,17 @@
 from bs4 import BeautifulSoup
-from deep_translator import GoogleTranslator
+from deep_translator import MyMemoryTranslator
 import os
 import shutil
 import time
 
 os.makedirs('output', exist_ok=True)
 
-nombre_archivo = 'index.html' # Cambia esto si tu archivo se llama distinto
+nombre_archivo = 'index.html' 
 
 with open(nombre_archivo, 'r', encoding='utf-8') as f:
     soup = BeautifulSoup(f, 'html.parser')
 
-translator = GoogleTranslator(source='es', target='en')
+translator = MyMemoryTranslator(source='es', target='en')
 etiquetas_a_traducir = soup.find_all(['p', 'h1', 'h2', 'h3', 'span', 'li'])
 
 for tag in etiquetas_a_traducir:
@@ -20,7 +20,6 @@ for tag in etiquetas_a_traducir:
         try:
             texto_traducido = translator.translate(texto_original)
             tag.string.replace_with(texto_traducido)
-            # Pausa de 1 segundo para que Google no nos bloquee
             time.sleep(1)
         except Exception as e:
             print(f"Aviso: No se pudo traducir: '{texto_original}'.")
